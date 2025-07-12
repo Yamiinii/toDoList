f5ff6f2c8f76c47be3d27188a32873580a0de853
Yamini <yamini6375@gmail.com>
2025-07-12 09:21:17 +0530

    added local storage

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+window.addEventListener("DOMContentLoaded",loadTasks)
 document.getElementById("addTaskBtn").addEventListener("click",addTask)
 
 function addTask(){
@@ -6,26 +7,67 @@ function addTask(){
 
     if(tasktext=="" ) return;
 
-    const li=document.createElement("li");
-    li.textContent=tasktext;
+    const task={
+        text:tasktext,
+        done:false
+    }
 
-    const doneBtn= document.createElement("button")
+    addTaskToDOM(task);
+    saveTask(task);
+    input.value="";
+
+}
+
+function addTaskToDOM(task){
+    const li=document.createElement("li")
+    li.textContent=task.text;
+
+    const doneBtn=document.createElement("button")
     doneBtn.textContent="Done"
     doneBtn.onclick=()=>{
-        li.style.textDecoration="line-through"
+        li.style.textDecoration="line-through";
         doneBtn.disabled=true
+        updateTask(task.text,true)
     }
+ const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "❌ Delete";
+  deleteBtn.onclick = () => {
+    li.remove();
+    deleteTask(task.text);
+  };
 
-    const deleteBtn=document.createElement("button")
-    deleteBtn.textContent="Delete"
-    deleteBtn.onclick=()=>{
-        li.remove();
-    }
+   if (task.done) {
+    li.style.textDecoration = "line-through";
+    doneBtn.disabled = true;
+  }
 
-    li.appendChild(doneBtn)
-    li.appendChild(deleteBtn)
+  li.appendChild(doneBtn);
+  li.appendChild(deleteBtn);
 
-    document.getElementById("taskList").appendChild(li)
-    input.value=""
+  document.getElementById("taskList").appendChild(li);
+}
 
+function saveTask(task){
+    const tasks=JSON.parse(localStorage.getItem("tasks") || "[]")
+    tasks.push(task)
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+}
+
+function deleteTask(text) {
+  let tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+  tasks = tasks.filter((t) => t.text !== text);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function updateTask(text, isDone) {
+  let tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+  tasks = tasks.map((t) =>
+    t.text === text ? { ...t, done: isDone } : t
+  );
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks() {
+  const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+  tasks.forEach(addTaskToDOM);
 }
